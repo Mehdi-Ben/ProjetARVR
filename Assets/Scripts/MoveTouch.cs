e98c77ee080b90109d46f3821441d2cd44cb5d01
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-11-23 16:03:25 +0100

    Mise en place de la gestion des inputs

---
+++ Assets/Scripts/MoveTouch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveTouch : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string input;
+    public int value;
+
+
+    private void OnPointerEnter()
+    {
+        
+        Tank.input[input] = value;
+    }
+  
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Tank.input[input] = value;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Tank.input[input] = 0;
+    }
+}
--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,26 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Tank : MonoBehaviour {
+public class Tank : NetworkBehaviour {
+    static public Dictionary<string, int> input = new Dictionary<string, int>();
+    public float speed;
+
     
-	public void forward()
+    private void Update()
     {
-       transform.position += Vector3.forward;
+        /*  if (forwardTouch.activate) forward();
+          if (backwardTouch.activate) backward();
+          if (leftTouch.activate) left();
+          if (rightTouch.activate) right();*/
+        if (!Tank.input.ContainsKey("AxisVertical"))
+        {
+            Tank.input.Add("AxisVertical", 0);
+            Tank.input.Add("AxisHorizontal", 0);
+        }
+        if (isLocalPlayer)
+        {
+            transform.position += (Vector3.forward * input["AxisVertical"] + Vector3.right * input["AxisHorizontal"]).normalized * Time.deltaTime * speed;
+        }
+
+    }
+
+    public void forward()
+    {
+       transform.position += Vector3.forward * Time.deltaTime;
     }
 
     public void backward()
     {
-        transform.position += -1 * transform.forward;
+        transform.position += -1 * transform.forward * Time.deltaTime;
     }
 
     public void left()
     {
-        transform.position += -1 * transform.right;
+        transform.position += -1 * transform.right * Time.deltaTime;
     }
 
    public  void right()
     {
-        transform.position += transform.right;
+        transform.position += transform.right * Time.deltaTime;
     }
 }
