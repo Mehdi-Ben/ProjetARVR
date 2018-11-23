5dad65e73d31f580805c8b368c8c2ec7c8f83a08
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-11-23 14:35:36 +0100

    Ajout des prefabs canvas (boutons pour bouger) et tank (carré représentant le tank qui bouge en appuyant sur les boutons)

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+    
+	public void forward()
+    {
+       transform.position += Vector3.forward;
+    }
+
+    public void backward()
+    {
+        transform.position += -1 * transform.forward;
+    }
+
+    public void left()
+    {
+        transform.position += -1 * transform.right;
+    }
+
+   public  void right()
+    {
+        transform.position += transform.right;
+    }
+}
